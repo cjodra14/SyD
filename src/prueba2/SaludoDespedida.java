bc4fa156cb47c6b0dc9622e74e07fd064269a8f7
multi <multi@A1EQUIPO09>
2021-03-05 19:43:13 +0100

    Crear

---
+++ src/prueba2/SaludoDespedida.java
@@ -0,0 +1,14 @@
+package prueba2;
+import prueba1.*;
+public class SaludoDespedida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Saludo hola=new Saludo();
+		Despedida adios=new Despedida();
+		hola.saludar();
+		adios.despedir();
+		
+	}
+
+}
